b7d37b6398ea2ac4f5a64f11a9cfe673e24838b6
关尔先生 <61576201+ZhengXinquan@users.noreply.github.com>
2020-02-28 16:07:47 +0800

    Add files via upload

---
+++ php/sql.php
@@ -0,0 +1,8 @@
+<?php
+date_default_timezone_set("Asia/Chongqing");
+//$servername = "localhost";$username = "root";$password = "root";$dbname = "****";$dbname = "hdm189315162_db";
+
+$servername="hdm189315162.my3w.com";$username="****";$password="****"; $dbname="****";
+
+// 设置编码，防止中文乱码//mysqli_query($conn , "set names utf8");
+?>
